5f98cb72e02c89f16aacb21bd657ccce07f0ea0c
Muhammad-Ali-Murtaza <muhammadalimurtaza997@gmail.com>
2021-04-04 23:27:24 +0500

    I have created jFrames

---
+++ src/Driver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Ali Murtaza
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fram1 f = new Fram1(f);
+        f.show();
+    }
+    
+}
